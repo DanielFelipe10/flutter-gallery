06cec33a103bf0f8aceccb69ace708f1e9e3ee78
DanielFelipe10 <daniel.tovart@uniagustiniana.edu.co>
2023-10-14 23:13:28 -0500

    create list of images

---
+++ lib/data/images_list.dart
@@ -0,0 +1,14 @@
+List images = [
+  "https://images.pexels.com/photos/2129796/pexels-photo-2129796.png?auto=compress&cs=tinysrgb&w=600",
+  "https://images.pexels.com/photos/1563256/pexels-photo-1563256.jpeg?auto=compress&cs=tinysrgb&w=600",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+];
