5abc79e3ed17f27a0e23fe692052fa2b7b72b940
DanielFelipe10 <daniel.tovart@uniagustiniana.edu.co>
2023-10-15 18:53:24 -0500

    add bottom bar in image view and like button

--- lib/pages/image_view.dart
+++ lib/pages/image_view.dart
@@ -6,8 +6,48 @@ class ImageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Image.network(url),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.arrow_back_sharp,
+                  size: 35.0,
+                )),
+            Center(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(5.0),
+                child: Image.network(
+                  url,
+                  height: 600.0,
+                  width: 360.0,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          child: Container(height: 50.0),
+        ),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Like',
+          child: const Icon(Icons.favorite_outlined,
+              color: Colors.red, size: 35.0),
+          backgroundColor: Colors.white,
+          onPressed: () {
+            // Tu función de manejo del botón aquí
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      ),
     );
   }
 }
